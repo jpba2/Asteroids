93e38f53bdd0105d8d02c5c35d3a953cc75bc972
JPBA <jpb@a.com>
2025-11-06 20:02:41 +0000

    framerate throttle

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 
